54ef44f75d5faf30a8ba3ba0b949d01433db0dbc
shohan12727 <aishohan001@gmail.com>
2025-03-11 15:01:48 +0600

    argument in function

--- index.js
+++ index.js
@@ -13,3 +13,52 @@
 // }
 // const result = sum(52,96);
 // console.log(result);
+
+// const name = "Ashraful Islam Shohan";
+// if (name){
+//     console.log("this is truty value");
+// }
+// else{
+//     console.log("this is falsy value");
+// }
+
+
+// if (10=='10'){
+//     console.log("true");
+// }
+// else {
+//    console.log("false");
+// }
+
+
+// if (10==='10'){
+//     console.log("true");
+// }
+// else {
+//    console.log("false");
+// }
+
+
+// function sum (){
+//     let counter = 0 ;
+//     counter ++;
+//     console.log(counter);
+// }
+// sum()
+
+
+// function numbers (a,b,c){
+//     console.log (a,b,c);
+// }
+// numbers(1,25,3);
+
+
+// function numbers (a,b,c){
+//     console.log (arguments);
+// }
+// numbers(1,25,3);
+
+function numbers (a,b,c){
+    console.log (...arguments);
+}
+numbers(1,25,3,65);
