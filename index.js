b5a39cfda6935e1faa4e50db74a52256df831cd6
shohan12727 <aishohan001@gmail.com>
2025-03-11 11:47:40 +0600

    updated

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+// let person = {
+//     name: "shohan"
+// }
+
+// let newPerson = "person";
+// person.name = "rakib";
+
+// const name = "Ashraful Islam Shohan";
+// console.log(typeof name);
+
+// function sum (num1,num2){
+//     return num1+num2;
+// }
+// const result = sum(52,96);
+// console.log(result);
